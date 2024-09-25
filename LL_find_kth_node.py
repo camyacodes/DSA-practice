9047c77a22f38aafe050c7a08d0c8756ac7503ec
Camya Robinson <camyacodes@gmail.com>
2024-09-25 17:25:46 -0400

    find kth node from end LL ?

--- LL_find_kth_node.py
+++ LL_find_kth_node.py
@@ -20,35 +20,41 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         return True
-    def find_middle_node(self):
-        slow = self.head
-        fast = self.head
-        while fast is not None and fast.next is not None:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
-    
-    
-
+  
+  
+        
+def find_kth_from_end(list, k):
+    slow = list.head
+    fast = list.head
+    for _ in range(k - 1):
+        if fast is None:
+            return None
+        fast = fast.next
+    while fast.next is not None:
+        fast = fast.next
+        slow = slow.next
+    return slow
 
 
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
-my_linked_list.append(5)
-my_linked_list.append(0)
-my_linked_list.append(2)
 my_linked_list.append(4)
-my_linked_list.append(9)
+my_linked_list.append(5)
 
-print( my_linked_list.find_middle_node().value )
+
+k = 2
+result = find_kth_from_end(my_linked_list, k)
+
+print(result.value)  # Output: 4
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    3
+    4
     
 """
+
