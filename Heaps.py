800309afb8ef4611b1e4d55857c67bbd3da9cd14
Camya Robinson <camyacodes@gmail.com>
2024-10-07 17:00:14 -0400

    max heap create method

---
+++ Heaps.py
@@ -0,0 +1,39 @@
+class maxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self,index):
+        return 2 * index + 1
+    
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, val):
+        self.heap.append(val)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+my_heap = maxHeap()
+my_heap.insert(99)
+my_heap.insert(72)
+my_heap.insert(61)
+my_heap.insert(58)
+
+print(my_heap.heap)
+
+my_heap.insert(100)
+
+print(my_heap.heap)
+
+my_heap.insert(100)
+
+print(my_heap.heap)
